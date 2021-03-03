ec16cb7f26b877bc8e685e2f17dab67f766a17c3
dahyeyudev <dahye.yu.dev@gmail.com>
2021-03-03 13:17:38 +0900

    초기세팅

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+    </>
+  );
+};
+
+export default App;
